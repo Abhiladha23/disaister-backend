3e811d52dc43900ae10f2ea4fb7e3503ffac1323
Abhiladha23 <abhiladha23@gmail.com>
2026-03-03 02:54:40 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,49 +2,27 @@ from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from database import Base
 from datetime import datetime
 
-
 class Incident(Base):
     __tablename__ = "incidents"
 
-    id = Column(Integer, primary_key=True, index=True)
-    message = Column(String, nullable=False)
-    lat = Column(Float, nullable=False)
-    lng = Column(Float, nullable=False)
-    disaster_type = Column(String, nullable=False)
-    severity = Column(Float, nullable=False)
-    risk_level = Column(String, nullable=False)
-    confidence = Column(Float, nullable=False)
-    medical_needed = Column(String, nullable=False)
-    satellite_verified = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True)
+    message = Column(String)
+    lat = Column(Float)
+    lng = Column(Float)
+    disaster_type = Column(String)
+    severity = Column(Float)
+    risk_level = Column(String)
+    confidence = Column(Float)
+    medical_needed = Column(String)
+    satellite_verified = Column(Boolean)
     timestamp = Column(DateTime, default=datetime.utcnow)
-
 
 class SOS(Base):
     __tablename__ = "sos"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    contact = Column(String, nullable=False)
-    lat = Column(Float, nullable=False)
-    lng = Column(Float, nullable=False)
-    timestamp = Column(DateTime, default=datetime.utcnow)
-
-
-class Action(Base):
-    __tablename__ = "actions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    action_type = Column(String, nullable=False)
-    lat = Column(Float, nullable=False)
-    lng = Column(Float, nullable=False)
-    timestamp = Column(DateTime, default=datetime.utcnow)
-
-
-class MonitoredLocation(Base):
-    __tablename__ = "monitored_locations"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    lat = Column(Float, nullable=False)
-    lng = Column(Float, nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    contact = Column(String)
+    lat = Column(Float)
+    lng = Column(Float)
     timestamp = Column(DateTime, default=datetime.utcnow)
